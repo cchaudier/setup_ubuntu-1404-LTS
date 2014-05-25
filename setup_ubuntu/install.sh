2a60c70b9b398acbe0a1a34e052783e5600ddfbe
Chaudier Christophe <chaudier.christophe@gmail.com>
2014-05-25 18:46:02 +0200

    add foxlor and phpmyadmin

--- setup_ubuntu/install.sh
+++ setup_ubuntu/install.sh
@@ -36,6 +36,8 @@ install_packages() {
   trace "Installation packages"
   run sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
   sudo add-apt-repository 'deb http://ftp.igh.cnrs.fr/pub/mariadb/repo/10.0/ubuntu trusty main'
+  run sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key FD88018B6F2D5390D051343FF6B4A8704F9E9BBC
+  sudo add-apt-repository 'deb http://debian.froxlor.org wheezy main'
   #run sudo 'echo "deb http://archive.ubuntu.com/ubuntu precise main universe" >> /etc/apt/sources.list'
   run sudo apt-get update 
   run sudo apt-get upgrade -y 
@@ -44,22 +46,24 @@ install_packages() {
   install_package language-pack-fr
   install_rcm
   install_lemp
+  install_panel
   trace " --> packages OK"
 }
 
 install_lemp() {
   trace "Installation stack LEMP"
-  install_package nginx php5 php5-fpm php-apc php5-mysql
-  run sudo mkdir -p /usr/share/nginx/www/$sever_name
+  install_package nginx php5 php5-fpm php-apc php5-mysql php5-curl
+  run sudo mkdir -p $web_rootpath/$sever_name
   sudo rm /etc/nginx/sites-enabled/default
   run sudo cp /app/setup_ubuntu/nginx.conf /etc/nginx/sites-available/$server_name
   run sudo ln -fs /etc/nginx/sites-available/$server_name /etc/nginx/sites-enabled/$server_name
-  run sudo cp /app/setup_ubuntu/info.php /usr/share/nginx/www/$server_name
+  #run sudo cp /app/setup_ubuntu/info.php /usr/share/nginx/www/$server_name
   run sudo service php5-fpm restart
   trace "  -> PHP OK"
   run sudo nginx -t && sudo service nginx restart
   trace "  -> nginx OK"
   install_mariadb
+  install_phpmyadmin
   trace " --> lemp OK"
 }
 
@@ -67,6 +71,13 @@ install_mariadb() {
   install_package mariadb-server
   run mysql -V 
   trace "  -> mariadb OK"
+}
+
+install_phpmyadmin() {
+  install_package phpmyadmin
+  #run sudo htpasswd /etc/phpmyadmin/htpasswd.setup admin
+  run sudo ln -s /usr/share/phpmyadmin/ /var/www/phpmyadmin
+  trace "  -> phpmyadmin OK"
 }
 
 install_package() {
@@ -77,8 +88,15 @@ install_rcm() {
     trace "Installation rcm"
     cd /tmp
     run wget http://thoughtbot.github.io/rcm/debs/rcm_1.2.0_all.deb
-    run sudo dpkg -i rcm_1.2.0_all.deb rm -f rcm_1.2.0_all.deb
+    run sudo dpkg -i rcm_1.2.0_all.deb 
+    rm -f rcm_1.2.0_all.deb
     cd -
+  trace "  -> rcm OK"
+}
+
+install_panel() {
+  install_package froxlor
+  trace "  -> Froxlor OK"
 }
 
 create_users() {
